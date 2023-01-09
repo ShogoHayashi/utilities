1ba731d1daee73abec009e2450752d17027d51b3
Shogo,Hayashi <20t0816a@student.gs.chiba-u.jp>
2023-01-10 00:16:32 +0900

    add input

--- FileIO/csv_io.py
+++ FileIO/csv_io.py
@@ -1,4 +1,17 @@
 import csv
 import pathlib
+import pprint
 
 
+# csvファイルの読み込み(open())
+def input_csv_1(csv_file):
+    with open(csv_file) as f:
+        pprint.pprint(f.read())
+        
+
+# csvファイルの読み込み(コンストラクタ使用)
+def input_csv_2(csv_file):
+    with open(csv_file) as f:
+        reader = csv.reader(f)
+        for row in reader:
+            print(row)
