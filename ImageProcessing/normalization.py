0976af8ed1bd064d2a5ca6ab63a91e8be117c810
Shogo,Hayashi <20t0816a@student.gs.chiba-u.jp>
2023-03-07 21:28:33 +0900

    normalization

--- ImageProcessing/normalization.py
+++ ImageProcessing/normalization.py
@@ -1,4 +1,8 @@
+import argparse
+import cv2
 import numpy as np
+
+
 
 def do_normalization(src_img, src_range, dst_range):
     dst_img = np.copy(src_img)
@@ -13,3 +17,20 @@ def do_normalization(src_img, src_range, dst_range):
     dst_img[dst_img > dst_range[1]] = dst_range[1]
 
     return dst_img.astype(np.uint8)
+
+
+def read_image(image_data_path, save_file_name='sample.png'):
+    img = cv2.imread(image_data_path)
+    img = do_normalization(img, [-150, 250], [0, 255])
+    cv2.imwrite(save_file_name, img)
+
+
+if __name__ == '__main__':
+        
+    parser = argparse.ArgumentParser()
+    parser.add_argument('image_data_path') 
+    parser.add_argument('--save_file_name', default = 'sample.png')           
+    
+    args = parser.parse_args()
+    
+    read_image(args.image_data_path, args.save_file_name)
